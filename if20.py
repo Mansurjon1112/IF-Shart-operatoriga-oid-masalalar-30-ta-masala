02ffdccfb110d73862fe0d7698b68985cbf42050
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-07 14:57:33 +0500

    Update if20.py

--- if20.py
+++ if20.py
@@ -8,14 +8,14 @@ Created on Sat Jun 26 18:13:10 2021
 @author: Mansurjon Kamolov
 """
 
-a=float(input('A='))
-b=float(input('B='))
-c=float(input('C='))
-
-ab =abs(a-b)
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+ab=abs(a-b)
 ac=abs(a-c)
-
 if ab<ac:
-    print('B nuqta yaqin',ab,'masofada')
-else:
-    print('C nuqta yaqin',ac,'masofada')
+    print('b nuqta',ab,'masofa')
+elif ab>ac:
+    print('c nuqta',ac,'masofa')
+else: 
+    print('Bir xil masofa')
